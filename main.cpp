8ef575249d5466b3cdd7db6202e200c9054166d0
Christopher Shi <christopher.shi22@gmail.com>
2020-05-09 03:21:18 +1000

    render a sphere

--- main.cpp
+++ main.cpp
@@ -3,10 +3,27 @@
 
 #include <iostream>
 
+// calculates roots of intersection
+// center = center of sphere
+// radius = radius of sphere
+// {quadratic formula}
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+	vec3 oc = r.origin() - center;
+	auto a = dot(r.direction(), r.direction());
+	auto b = 2.0 * dot(oc, r.direction());
+	auto c = dot(oc, oc) - radius*radius;
+	auto discriminant = b*b - 4*a*c;
 
+	return (discriminant > 0);
+}
+
+// {linear interpolation formula}
 // linear interpolation of ray colors in a unidirectional gradient
 color ray_color(const ray& r) {
 	vec3 unit_direction = unit_vector(r.direction());
+	if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
+		return color(1, 0, 0);
+	}
 	auto t = 0.5*(unit_direction.y() + 1.0);
 	return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
 }
